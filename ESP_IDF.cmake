6e51b2038eb1e938476900a6d28e8a44fb2dba16
RobotMan2412 <julian@scheffers.net>
2023-03-09 13:29:39 +0100

    Disable a nasty ESP-IDF error.

--- ESP_IDF.cmake
+++ ESP_IDF.cmake
@@ -20,3 +20,4 @@ idf_component_register(
 	"src/fonts/font_bitmap_sairacondensed.c"
 	INCLUDE_DIRS src src/cpp
 )
+set_target_properties(${COMPONENT_LIB} PROPERTIES COMPILE_FLAGS -Wno-unused-const-variable)
